472fb904465dd0980798b841f1c280c81820a413
João Victor Mendes <joaodev21@gmail.com>
2022-08-14 08:19:58 -0500

    #21 Identificar bug no arquivo main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,4 +38,44 @@ const skillsContent = document.getElementsByClassName('skills__content'),
         el.addEventListener('click', toggleSkills)
       })
 
+      /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+tab.addEventListener('click', () =>{
+  const target = document.querySelector(tab.dataset.target)
+
+  tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('qualification__active')
+  })
+  target.classList.add('qualification__active')
+
+  tabs.forEach(tab =>{
+      tab.classList.remove('qualification__active')
+  })
+  tab.classList.add('qualification__active')
+})
+})
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
       
